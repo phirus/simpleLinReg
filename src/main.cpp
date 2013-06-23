c652fde6478b09d2938434bc4cdc15f82a742369
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-06-23 16:45:00 +0200

    added options for input and output files

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include<string>
 #include<boost/program_options.hpp>
+
+#include"linReg.h"
 
 using namespace std;
 
@@ -9,6 +12,8 @@ int main(int argc, char** argv){
 	boost::program_options::options_description desc("Allowed options");
 	desc.add_options()
         ("help,h", "produce help message")
+        ("input,i", boost::program_options::value<string> (), "specify input file")
+        ("output,o", boost::program_options::value<string> (), "specify output file")
         ;
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc,argv,desc),vm);
@@ -19,7 +24,23 @@ int main(int argc, char** argv){
         cout << desc << endl;
         return 1;
     }
+
+    if (vm.count("input")) {
+        cout << "Input file is: " << vm["input"].as<string>() << ".\n" << endl ;
+        LinReg lr = readFile(vm["input"].as<string>());
+        cout << "a = " << lr.getA() << endl;
+        cout << "b = " << lr.getB()<< endl;
+        cout << "sigma = " << lr.getSigma() << endl;    
+
+        if (vm.count("output")) {
+            lr.writeToFile(vm["output"].as<string>());
+            cout << "Output written to " << vm["output"].as<string>() << endl;
+        } else cout << "no Output file specified" << endl;
+
+    } else {
+    cout << "Input file was not set.\n";
+    }
     
-std::cout << "Hello World" << std::endl;
+
 return 0;
 }
